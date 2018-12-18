b7d34302fd7f2a19f39ab70d03231161f00cf347
Fernandinho <johfermq@yahoo.com>
2018-12-18 18:04:58 -0500

    first commit

---
+++ app/Models/Favorite.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Favorite extends Model
+{
+  	/*
+  	|----------------------------------------------------------------------
+  	| My relations
+  	|----------------------------------------------------------------------
+  	|
+  	*/
+
+  	tweet () {
+    	return this.belongsTo('App/Models/Tweet')
+	}
+
+	user () {
+    	return this.belongsTo('App/Models/User')
+	}
+
+}
+
+module.exports = Favorite
---
+++ app/Models/Tweet.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Tweet extends Model
+{
+  	/*
+	|----------------------------------------------------------------------
+	| My relations
+	|----------------------------------------------------------------------
+	|
+	*/
+
+	user () {
+    	return this.belongsTo('App/Models/User')
+	}
+
+	replies () {
+    	return this.hasMany('App/Models/Reply')
+	}
+
+	favorites () {
+    	return this.hasMany('App/Models/Favorite')
+	}
+
+}
+
+module.exports = Tweet
